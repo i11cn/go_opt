463e87822ce23595cc25a4eed6699148937f3a2d
snower <snower@snower.org>
2015-08-30 23:06:50 +0800

    大体上知道要些啥接口了

--- option.go
+++ option.go
@@ -2,13 +2,17 @@ package option
 
 import (
 	"encoding/json"
+	"fmt"
+	"strconv"
 )
 
 type (
 	Options struct {
-		Name  string
-		Value interface{}
+		value interface{}
+		items map[string]*Options
 	}
+
+	opt_dump map[string]interface{}
 )
 
 func NewOptions() *Options {
@@ -16,7 +20,8 @@ func NewOptions() *Options {
 }
 
 func (o *Options) Dump() string {
-	data, err := json.Marshal(o)
+	d := o.dump()
+	data, err := json.Marshal(d)
 	if err != nil {
 		return ""
 	} else {
@@ -24,36 +29,152 @@ func (o *Options) Dump() string {
 	}
 }
 
-func (o *Options) MustInt(path ...string) bool {
-	return false
+func (o *Options) Test() {
+	o.items = make(map[string]*Options)
+	o.items["name"] = &Options{15.5, nil}
+	o.items["test"] = &Options{nil, make(map[string]*Options)}
+	o.items["test"].items["aaa"] = &Options{true, nil}
 }
 
-func (o *Options) MustIntWithRange(min, max int64, path ...string) bool {
-	return false
+func (o *Options) Get(path ...string) *Options {
+	if o == nil || len(path) == 0 {
+		return o
+	}
+	cur := o
+	var ok bool
+	for _, p := range path {
+		if cur.items == nil {
+			return nil
+		}
+		if cur, ok = cur.items[p]; !ok {
+			return nil
+		}
+	}
+	return cur
 }
 
-func (o *Options) MustFloat(path ...string) bool {
-	return false
+func (o *Options) Exist(path ...string) bool {
+	return o.Get(path...) != nil
 }
 
-func (o *Options) MustFloatWithRange(min, max float64, path ...string) bool {
-	return false
+func (o *Options) MustInt() bool {
+	if o.value == nil {
+		return false
+	}
+	switch v := o.value.(type) {
+	case int64:
+		return true
+	case string:
+		use, err := strconv.ParseInt(v, 10, 64)
+		if err == nil {
+			o.value = use
+			return true
+		} else {
+			return false
+		}
+	case float64:
+		o.value = int64(v)
+		return true
+	default:
+		return false
+	}
 }
 
-func (o *Options) MustBool(path ...string) bool {
-	return false
+func (o *Options) MustIntWithRange(min, max int64) bool {
+	if !o.MustInt() {
+		return false
+	}
+	if v, ok := o.value.(int64); ok {
+		return v >= min && v <= max
+	} else {
+		return false
+	}
 }
 
-func (o *Options) Get(path ...string) string {
-	return ""
+func (o *Options) MustFloat() bool {
+	if o.value == nil {
+		return false
+	}
+	switch v := o.value.(type) {
+	case float64:
+		return true
+	case string:
+		use, err := strconv.ParseFloat(v, 64)
+		if err == nil {
+			o.value = use
+			return true
+		} else {
+			return false
+		}
+	case int64:
+		o.value = float64(v)
+		return true
+	default:
+		return false
+	}
 }
 
-func (o *Options) GetInt(path ...string) int64 {
-	return 0
+func (o *Options) MustFloatWithRange(min, max float64) bool {
+	if !o.MustFloat() {
+		return false
+	}
+	if v, ok := o.value.(float64); ok {
+		return v >= min && v <= max
+	} else {
+		return false
+	}
 }
 
-func (o *Options) GetFloat(path ...string) float64 {
-	return 0.0
+func (o *Options) MustBool() bool {
+	if o.value == nil {
+		return false
+	}
+	switch v := o.value.(type) {
+	case bool:
+		return true
+	case string:
+		use, err := strconv.ParseBool(v)
+		if err == nil {
+			o.value = use
+			return true
+		} else {
+			return false
+		}
+	default:
+		return false
+	}
+}
+
+func (o *Options) String() string {
+	if ret, ok := o.value.(string); ok {
+		return ret
+	} else {
+		return fmt.Sprint(o.value)
+	}
+}
+
+func (o *Options) Int() int64 {
+	if ret, ok := o.value.(int64); ok {
+		return ret
+	} else {
+		return 0
+	}
+}
+
+func (o *Options) Float() float64 {
+	if ret, ok := o.value.(float64); ok {
+		return ret
+	} else {
+		return 0.0
+	}
+}
+
+func (o *Options) Bool() bool {
+	if ret, ok := o.value.(bool); ok {
+		return ret
+	} else {
+		return false
+	}
 }
 
 func (o *Options) ParseJsonFile(path string) error {
@@ -61,7 +182,7 @@ func (o *Options) ParseJsonFile(path string) error {
 }
 
 func (o *Options) ParseJson(js string) error {
-	return nil
+	return o.parse_json_string(js)
 }
 
 func (o *Options) ParseCommand() {
