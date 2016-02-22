218ac02d79b21365abbc7a9e6db28af1b5d00912
snower <snower@snower.org>
2016-02-23 00:17:19 +0800

    开始写具体的实现”

--- option.go
+++ option.go
@@ -1,188 +1,103 @@
 package option
 
 import (
-	"encoding/json"
 	"fmt"
-	"strconv"
 )
 
 type (
 	Options struct {
 		value interface{}
-		items map[string]*Options
+		child map[string]*Options
 	}
-
-	opt_dump map[string]interface{}
 )
 
 func NewOptions() *Options {
 	return &Options{}
 }
 
-func (o *Options) Dump() string {
-	d := o.dump()
-	data, err := json.Marshal(d)
-	if err != nil {
-		return ""
-	} else {
-		return string(data)
-	}
-}
-
-func (o *Options) Test() {
-	o.items = make(map[string]*Options)
-	o.items["name"] = &Options{15.5, nil}
-	o.items["test"] = &Options{nil, make(map[string]*Options)}
-	o.items["test"].items["aaa"] = &Options{true, nil}
+func (o *Options) Marshal() (ret string, err error) {
+	return
 }
 
 func (o *Options) Get(path ...string) *Options {
 	if o == nil || len(path) == 0 {
 		return o
 	}
-	cur := o
-	var ok bool
-	for _, p := range path {
-		if cur.items == nil {
-			return nil
-		}
-		if cur, ok = cur.items[p]; !ok {
-			return nil
-		}
-	}
-	return cur
+	return nil
 }
 
 func (o *Options) Exist(path ...string) bool {
 	return o.Get(path...) != nil
 }
 
-func (o *Options) MustInt() bool {
-	if o.value == nil {
-		return false
-	}
-	switch v := o.value.(type) {
-	case int64:
-		return true
-	case string:
-		use, err := strconv.ParseInt(v, 10, 64)
-		if err == nil {
-			o.value = use
-			return true
-		} else {
-			return false
-		}
-	case float64:
-		o.value = int64(v)
-		return true
-	default:
-		return false
-	}
-}
-
-func (o *Options) MustIntWithRange(min, max int64) bool {
-	if !o.MustInt() {
-		return false
-	}
-	if v, ok := o.value.(int64); ok {
-		return v >= min && v <= max
-	} else {
-		return false
-	}
-}
-
-func (o *Options) MustFloat() bool {
-	if o.value == nil {
-		return false
-	}
-	switch v := o.value.(type) {
-	case float64:
-		return true
-	case string:
-		use, err := strconv.ParseFloat(v, 64)
-		if err == nil {
-			o.value = use
-			return true
-		} else {
-			return false
-		}
-	case int64:
-		o.value = float64(v)
-		return true
-	default:
-		return false
-	}
-}
-
-func (o *Options) MustFloatWithRange(min, max float64) bool {
-	if !o.MustFloat() {
-		return false
-	}
-	if v, ok := o.value.(float64); ok {
-		return v >= min && v <= max
-	} else {
-		return false
-	}
-}
-
-func (o *Options) MustBool() bool {
-	if o.value == nil {
-		return false
-	}
-	switch v := o.value.(type) {
-	case bool:
-		return true
-	case string:
-		use, err := strconv.ParseBool(v)
-		if err == nil {
-			o.value = use
-			return true
-		} else {
-			return false
-		}
-	default:
-		return false
-	}
-}
-
 func (o *Options) String() string {
-	if ret, ok := o.value.(string); ok {
-		return ret
-	} else {
-		return fmt.Sprint(o.value)
+	switch v := o.value.(type) {
+	case string:
+		return v
+	default:
+		return fmt.Sprint(v)
 	}
 }
 
-func (o *Options) Int() int64 {
-	if ret, ok := o.value.(int64); ok {
-		return ret
-	} else {
-		return 0
+func (o *Options) Int() (ret int, ok bool) {
+	ret = 0
+	ok = false
+	switch v := o.value.(type) {
+	case int:
+		ret = int(v)
+		ok = true
+	case uint:
+		ret = int(v)
+		ok = true
+	case int32:
+		ret = int(v)
+		ok = true
+	case uint32:
+		ret = int(v)
+		ok = true
+	case int64:
+		ret = int(v)
+		ok = true
+	case uint64:
+		ret = int(v)
+		ok = true
 	}
+	return
 }
 
-func (o *Options) Float() float64 {
-	if ret, ok := o.value.(float64); ok {
-		return ret
-	} else {
-		return 0.0
-	}
+func (o *Options) UInt() (ret uint, ok bool) {
+	return
 }
 
-func (o *Options) Bool() bool {
-	if ret, ok := o.value.(bool); ok {
-		return ret
-	} else {
-		return false
-	}
+func (o *Options) Int64() (ret int, ok bool) {
+	return
+}
+
+func (o *Options) UInt64() (ret uint, ok bool) {
+	return
+}
+
+func (o *Options) Float() (ret float64, ok bool) {
+	return
+}
+
+func (o *Options) Bool() (ret bool, ok bool) {
+	return
 }
 
 func (o *Options) ParseJsonFile(path string) error {
 	return nil
 }
 
+func (o *Options) Set(o2 *Options) {
+}
+
 func (o *Options) ParseJson(js string) error {
 	return o.parse_json_string(js)
+}
+
+func (o *Options) ParseIniFile(path string) error {
+	return nil
 }
 
 func (o *Options) ParseCommand() {
--- option_native.go
+++ option_native.go
@@ -4,34 +4,15 @@ import (
 	"github.com/bitly/go-simplejson"
 )
 
-func (o *Options) dump() opt_dump {
-	ret := opt_dump{}
-	if o.value != nil {
-		ret["."] = o.value
-	}
-	for k, v := range o.items {
-		ret[k] = v.dump()
-	}
-	return ret
-}
-
 func (o *Options) parse_json_object(j *simplejson.Json) error {
-	if s, err := j.String(); err == nil {
-		o.value = s
-	}
-	cur := j.GetIndex(0)
-	for idx := 1; cur != nil; {
-		cur = j.GetIndex(idx)
-		idx++
-	}
 	return nil
 }
+
 func (o *Options) parse_json_string(js string) error {
-	j, err := simplejson.NewJson([]byte(js))
-	if err != nil {
-		return err
-	}
-	return o.parse_json_object(j)
+	return nil
+}
+
+func (o *Options) parse_ini_file() error {
 	return nil
 }
 
